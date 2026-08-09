34aa74e1e524c0c1a228b3a5bf1d61466487a2d1
unknown <tanvirxyz11@gmail.com>
2026-08-10 05:01:45 +0800

    added the modal functionalities

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const displayLevel = (datas) => {
   for (const data of datas) {
     const level = document.createElement("div");
     level.innerHTML = `
-    <button onclick="wordLoad(${data.level_no})" class="btn   btn-primary btn-outline rounded-lg py-5 px-7">
+    <button id="wordLoad(${data.level_no})" onclick="wordLoad(${data.level_no})" class="btn  inactive btn-primary btn-outline rounded-lg py-5 px-7">
           <img src="./assets/fa-book-open.png" alt="" />Lesson-${data.level_no}
         </button>`;
     levelContainer.appendChild(level);
@@ -20,10 +20,15 @@ const wordLoad = (id) => {
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((allWords) => allWord(allWords.data));
+    .then((allWords) => {
+      const clickedBtn = document.getElementById(`wordLoad(${id})`);
+      const inactive = document.querySelectorAll(".inactive");
+      inactive.forEach((inact) => inact.classList.remove("active"));
 
-  //   const wordText = document.getElementById("word-text");
-  //   wordText.innerHTML = "";
+      clickedBtn.classList.add("active");
+
+      allWord(allWords.data);
+    });
 };
 const allWord = (words) => {
   const wordContainer = document.getElementById("words-container");
@@ -47,14 +52,14 @@ const allWord = (words) => {
     const newDiv = document.createElement("div");
 
     newDiv.innerHTML = `
-    <div class="border rounded-xl h-full shadow-lg p-8">
+    <div class="border rounded-xl h-full flex justify-between flex-col shadow-lg p-8">
         <div class="mb-2">
           <h4 class="text-2xl font-bold mb-3">${word.word ? word.word : "no word"}</h4>
           <p>Meaning/Pronounciation</p>
-          <h5 class="text-2xl mt-3 mb-3">${word.meaning ? word.meaning : "no word"}/${word.pronunciation ? word.pronunciation : "no word"}</h5>
+          <h5 class="text-2xl mt-3 mb-3 ">${word.meaning ? word.meaning : "no word"}/${word.pronunciation ? word.pronunciation : "no word"}</h5>
         </div>
-        <div class="flex justify-between items-center">
-          <span
+        <div class="flex justify-between items-center ">
+          <span id="${word.id}" onclick='loadWordDetails(${word.id})'
             class="flex justify-center items-center w-14 h-14 bg-blue-100 rounded-lg"
             ><i class="fa-solid fa-circle-info"></i
           ></span>
@@ -69,6 +74,38 @@ const allWord = (words) => {
 
     wordContainer.appendChild(newDiv);
   });
+};
+
+const loadWordDetails = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+
+  const res = await fetch(url);
+  const details = await res.json();
+  displayWordDetails(details.data);
+};
+
+const displayWordDetails = (details) => {
+  const modalText = document.getElementById("modal-text");
+  modalText.innerHTML = `
+     <div class="flex flex-col gap-1">
+      <h3 class="text-2xl font-bold">${details.word}(${details.pronunciation})</h3>
+
+      <p class="font-bold mt-3">Meaning</p>
+      <span>${details.meaning}</span>
+
+      <p class="font-bold mt-3">Example</p>
+      <span>${details.sentence}</span>
+
+      <p class="bangla-font font-bold mt-3">সমার্থক শব্দ গুলো</p>
+
+      <div class="flex gap-1">
+        <span class="p-2 rounded-lg bg-blue-200 font-black">${details.synonyms[0]}</span>
+        <span class="p-2 rounded-lg bg-blue-200 font-black">${details.synonyms[1]}</span>
+        <span class="p-2 rounded-lg bg-blue-200 font-black">${details.synonyms[2]}</span>
+      </div>
+    </div>
+  `;
+  document.getElementById("my_modal").showModal();
 };
 
 loadLesson();
