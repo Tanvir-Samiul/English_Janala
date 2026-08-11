c699e2815392335c82eec357f1d775900453b85a
unknown <tanvirxyz11@gmail.com>
2026-08-11 16:16:05 +0800

    added the search feature

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
+const synonymsBtn = (arr) => {
+  const htmlElements = arr.map(
+    (el) => `<span class="p-2 rounded-lg bg-blue-100 ">${el}</span>`,
+  );
+  return htmlElements.join(" ");
+};
+
+const loadSpiner = (status) => {
+  if (status == true) {
+    document.getElementById("spiner").classList.remove("hidden");
+    document.getElementById("whole-word-container").classList.add("hidden");
+  } else {
+    document.getElementById("whole-word-container").classList.remove("hidden");
+    document.getElementById("spiner").classList.add("hidden");
+  }
+};
+
+// loadLesson is a function to load the api of all levels and it sends the data to displayLevel funciton
+
 const loadLesson = () => {
   fetch("https://openapi.programming-hero.com/api/levels/all")
     .then((res) => res.json())
     .then((levels) => displayLevel(levels.data));
 };
+
+// displayLevel is a function to display all the levels , there are total 7 levels.
 
 const displayLevel = (datas) => {
   const levelContainer = document.getElementById("levels-container");
@@ -16,20 +37,28 @@ const displayLevel = (datas) => {
   }
 };
 
+// wordLoad is a funtion to load all the word, it tiggered when any level got clicked, from displayLevel funciton it's receive which button actually has clicked.
+// it also has a function named inactive which remove all the active class from all the button except the one clicked.
+
 const wordLoad = (id) => {
+  loadSpiner(true);
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
   fetch(url)
     .then((res) => res.json())
     .then((allWords) => {
       const clickedBtn = document.getElementById(`wordLoad(${id})`);
-      const inactive = document.querySelectorAll(".inactive");
-      inactive.forEach((inact) => inact.classList.remove("active"));
-
-      clickedBtn.classList.add("active");
+      const inactive = () => {
+        const inactiveBtn = document.querySelectorAll(".inactive");
+        inactiveBtn.forEach((inact) => inact.classList.remove("active"));
+        clickedBtn.classList.add("active");
+      };
+      inactive();
 
       allWord(allWords.data);
     });
 };
+
+// allWord is the function which display all the word or cards when any level is being clicked. it receive an array of all words.
 const allWord = (words) => {
   const wordContainer = document.getElementById("words-container");
   wordContainer.innerHTML = "";
@@ -45,6 +74,7 @@ const allWord = (words) => {
            Please Select Another Lesson
         </p>
       </div>`;
+    loadSpiner(false);
     return;
   }
 
@@ -52,7 +82,7 @@ const allWord = (words) => {
     const newDiv = document.createElement("div");
 
     newDiv.innerHTML = `
-    <div class="border rounded-xl h-full flex justify-between flex-col shadow-lg p-8">
+    <div class=" bg-white rounded-xl h-full flex justify-between flex-col shadow-lg p-8">
         <div class="mb-2">
           <h4 class="text-2xl font-bold mb-3">${word.word ? word.word : "no word"}</h4>
           <p>Meaning/Pronounciation</p>
@@ -74,7 +104,10 @@ const allWord = (words) => {
 
     wordContainer.appendChild(newDiv);
   });
+  loadSpiner(false);
 };
+
+// loadWordDetails is a function to fetch the words details api, it triggerd when information btn is been clicked in allword funciton. It sends an unique id of which info button is clicked
 
 const loadWordDetails = async (id) => {
   const url = `https://openapi.programming-hero.com/api/word/${id}`;
@@ -84,6 +117,7 @@ const loadWordDetails = async (id) => {
   displayWordDetails(details.data);
 };
 
+// displayWordDetails function actually display the word details card when info button clicked.
 const displayWordDetails = (details) => {
   const modalText = document.getElementById("modal-text");
   modalText.innerHTML = `
@@ -96,16 +130,31 @@ const displayWordDetails = (details) => {
       <p class="font-bold mt-3">Example</p>
       <span>${details.sentence}</span>
 
-      <p class="bangla-font font-bold mt-3">সমার্থক শব্দ গুলো</p>
+      <p class="bangla-font font-bold mt-3 mb-1">সমার্থক শব্দ গুলো</p>
 
       <div class="flex gap-1">
-        <span class="p-2 rounded-lg bg-blue-200 font-black">${details.synonyms[0]}</span>
-        <span class="p-2 rounded-lg bg-blue-200 font-black">${details.synonyms[1]}</span>
-        <span class="p-2 rounded-lg bg-blue-200 font-black">${details.synonyms[2]}</span>
+        ${synonymsBtn(details.synonyms)}
       </div>
     </div>
   `;
   document.getElementById("my_modal").showModal();
 };
+
+document.getElementById("btn-search").addEventListener("click", async () => {
+  const inactiveBtn = document.querySelectorAll(".inactive");
+  inactiveBtn.forEach((inact) => inact.classList.remove("active"));
+
+  const input = document.getElementById("input-search").value.toLowerCase();
+
+  const url = "https://openapi.programming-hero.com/api/words/all";
+  const res = await fetch(url);
+  const details = await res.json();
+  const allWords = details.data;
+  const filterWord = allWords.filter((word) =>
+    word.word.toLowerCase().includes(input),
+  );
+  allWord(filterWord);
+  document.getElementById("input-search").value = "";
+});
 
 loadLesson();
